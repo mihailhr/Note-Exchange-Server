6f50c9c748144c7c7e28842a03d8a46266d493e0
mihailhr <mishohristov3@gmail.com>
2024-10-14 12:28:19 +0300

    Connected to remote DB, tested out basic DB interactions

--- server.js
+++ server.js
@@ -1,15 +1,49 @@
 const express=require("express")
-
+require("dotenv").config()
+const {Pool}=require("pg")
 const app=express()
 const port=3000
 
 
+const pool=new Pool({
+    connectionString:process.env.CONNECTION_STRING,
+    ssl:{
+        rejectUnauthorized:false
+    }
+})
 
 app.get("/",(req,res)=>{
     try {
         res.status(200).send("Test endpoint is working")
     } catch (error) {
         res.status(400).send("Problem occurred")
+    }
+})
+
+app.get("/createUsersTable",async(req,res)=>{
+    try {
+        const client =await pool.connect()
+        const result=await client.query(`
+            CREATE TABLE IF NOT EXISTS Users(
+            User_id SERIAL PRIMARY KEY,
+            Username VARCHAR(15))`)
+        res.send("Created Users table")
+        await client.release()
+    } catch (error) {
+        console.log(error)
+        res.send("Something went wrong: " + error)
+    }
+})
+
+app.get("/insertFirstUsers",async (req,res)=>{
+    try {
+        const client=await pool.connect()
+        const result=await client.query("INSERT INTO Users (Username) VALUES ('Mihail'), ('George')")
+        res.send("Added first users")
+        client.release()
+    } catch (error) {
+        console.log(error)
+        res.send("Problem occurred: " + error)
     }
 })
 
